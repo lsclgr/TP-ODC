d41c8b5dc212f3797370632f4a17cba93e67fb06
Cecote <thiago.cecote@aluno.ufop.edu.br>
2021-11-29 10:15:35 -0300

    biblioteca time na MMU

--- TP2-ODC/MMU.c
+++ TP2-ODC/MMU.c
@@ -5,62 +5,68 @@
 
 MemoryBlock searchInMemories(Address add, MemoryBlock *RAM, MemoryBlock *cache1,
                              MemoryBlock *cache2, MemoryBlock *cache3) {
-    int cache1Position = (int)add.addBlock % sizeCache1;  // valor entre 0 e 15
-    int cache2Position = (int)add.addBlock % sizeCache2;  // valor entre 0 e 31
-    int cache3Position = (int)add.addBlock % sizeCache3;  // valor entre 0 e 63
+    // int cache1Position = (int)add.addBlock % sizeCache1;  // valor entre 0 e 15
+    // int cache2Position = (int)add.addBlock % sizeCache2;  // valor entre 0 e 31
+    // int cache3Position = (int)add.addBlock % sizeCache3;  // valor entre 0 e 63
     int cost = 0;
+    // ADICIONAR TEMPO
 
-    if (cache1[cache1Position].addBlock == add.addBlock) {
-        cost += 10;
-        cache1[cache1Position].cost = cost;
-        cache1[cache1Position].cacheHit = 1;
-        // cache1[cache1Position].updated = true
-        return cache1[cache1Position];
-    } else {
-        if (cache2[cache2Position].addBlock == add.addBlock) {
+    for (int i = 0; i < sizeCache1; i++) {
+        if (cache1[i].addBlock == add.addBlock) {
+            // ADICIONAR TEMPO
+
+            cost += 10;
+            cache1[i].cost = cost;
+            cache1[i].cacheHit = 1;
+            // cache1[cache1Position].updated = true
+            return cache1[i];
+        }
+    }
+    for (int i = 0; i < sizeCache2; i++) {
+        if (cache2[i].addBlock == add.addBlock) {
             cost += 110;
             // pq ele não colocou o custo?
-            cache2[cache2Position].cacheHit = 2;
+            cache2[i].cacheHit = 2;
             // cache2[cache2Position].updated = true
-            return CachesTest(cache1Position, cache2Position, cache3Position,
-                              cache1, cache2, cache3, cost, 1);
+            return CachesTest(i, cache1, cache2, cache3, cost, 1);
+        }
+    }
+
+    for (int i = 0; i < sizeCache3; i++) {
+        if (cache3[i].addBlock == add.addBlock) {
+            cost += 1110;
+            // pq ele não colocou o custo?
+            cache3[i].cacheHit = 3;
+            // cache3[cache3Position].updated = true
+            return CachesTest(i, cache1, cache2, cache3, cost, 0);
+        }
+    }
+    else {
+        // buscar na ram E O REPASSAR A C2 E DEPOIS A C1
+        cost += 11110;
+        if (!cache3[cache3Position].updated) {
+            cache3[cache3Position] = RAM[(int)add.addBlock];
+            cache3[cache3Position].cacheHit = 4;
+            return CachesTest(cache1Position, cache2Position,
+                              cache3Position, cache1, cache2, cache3,
+                              cost, 0);
+
         } else {
-            if (cache3[cache3Position].addBlock == add.addBlock) {
-                cost += 1110;
-                // pq ele não colocou o custo?
-                cache3[cache3Position].cacheHit = 3;
-                // cache3[cache3Position].updated = true
-                return CachesTest(cache1Position, cache2Position,
-                                  cache3Position, cache1, cache2, cache3, cost,
-                                  0);
-            } else {
-                // buscar na ram E O REPASSAR A C2 E DEPOIS A C1
-                cost += 11110;
-                if (!cache3[cache3Position].updated) {
-                    cache3[cache3Position] = RAM[(int)add.addBlock];
-                    cache3[cache3Position].cacheHit = 4;
-                    return CachesTest(cache1Position, cache2Position,
-                                      cache3Position, cache1, cache2, cache3,
-                                      cost, 0);
+            RAM[cache3[cache3Position].addBlock] = cache3[cache3Position];
+            RAM[cache3[cache3Position].addBlock].updated = false;  // virar false
 
-                } else {
-                    RAM[cache3[cache3Position].addBlock] = cache3[cache3Position];
-                    RAM[cache3[cache3Position].addBlock].updated = false;  // virar false
-
-                    cache3[cache3Position] = RAM[(int)add.addBlock];
-                    cache3[cache3Position].cacheHit = 4;
-                    return CachesTest(cache1Position, cache2Position,
-                                      cache3Position, cache1, cache2, cache3,
-                                      cost, 0);
-                    ;
-                }
-            }
+            cache3[cache3Position] = RAM[(int)add.addBlock];
+            cache3[cache3Position].cacheHit = 4;
+            return CachesTest(cache1Position, cache2Position,
+                              cache3Position, cache1, cache2, cache3,
+                              cost, 0);
+            ;
         }
     }
 }
+}
 
-MemoryBlock CachesTest(int cache1Position, int cache2Position,
-                       int cache3Position, MemoryBlock *cache1,
+MemoryBlock CachesTest(int i, MemoryBlock *cache1,
                        MemoryBlock *cache2, MemoryBlock *cache3, int cost,
                        int isCache2) {
     MemoryBlock aux;
